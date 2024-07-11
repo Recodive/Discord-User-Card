82e4ccf953f4d129ca877b4be1029540a39c3bf7
Bas950 <me@bas950.com>
2024-07-11 13:05:06 +0200

    feat(vue): export type

--- packages/vue/src/index.ts
+++ packages/vue/src/index.ts
@@ -1,3 +1,27 @@
 import DiscordUserCard from "./components/DiscordUserCard.vue";
 
 export { DiscordUserCard };
+export {
+	ActivityType,
+	type DiscordUserCardActivity,
+	type DiscordUserCardActivityAssets,
+	type DiscordUserCardActivityBase,
+	type DiscordUserCardActivityButton,
+	type DiscordUserCardActivityButtons,
+	type DiscordUserCardActivityCompeting,
+	type DiscordUserCardActivityCustom,
+	type DiscordUserCardActivityListening,
+	type DiscordUserCardActivityPlaying,
+	type DiscordUserCardActivityStreaming,
+	type DiscordUserCardActivityTimestamps,
+	type DiscordUserCardActivityWatching,
+	DiscordUserCardBadges,
+	DiscordUserCardBotBadges,
+	type DiscordUserCardImage,
+	type DiscordUserCardProperties,
+	type DiscordUserCardUser,
+	PresenceUpdateStatus,
+	type Snowflake,
+	defaultUserCardProperties,
+	flagsToBadges,
+} from "discord-user-card";
